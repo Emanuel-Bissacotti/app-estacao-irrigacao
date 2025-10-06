ea2b6301a2a378bb3714237f61223b8be195ca2d
Emanuel-Bissacotti <emanuelfbissacotti@gmail.com>
2025-10-06 15:41:32 -0300

    Adicionar arquivos para ajudar a banca a rodar o projeto

---
+++ app_estacao_irrigacao/scripts/run_dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "🚀 Iniciando em modo DESENVOLVIMENTO (com emuladores)..."
+flutter run -t lib/main_development.dart
---
+++ app_estacao_irrigacao/scripts/run_prod.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "🌐 Iniciando em modo PRODUÇÃO (servidor Firebase)..."
+flutter run -t lib/main_production.dart
